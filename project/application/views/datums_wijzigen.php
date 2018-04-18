beea404ea9421726696e035083c5dba173ce2af0
Kim Moelants <r0615534@student.thomasmore.be>
2018-04-18 14:30:16 +0200

    datums wijzigen final

--- project/application/views/datums_wijzigen.php
+++ project/application/views/datums_wijzigen.php
@@ -6,20 +6,6 @@
     $datumlijst = "";
     $i = 1;
 ?>
-<<<<<<< HEAD
-<div class="container">
-    <div class="row">
-        <div class="col-md-12 ">
-            <div class="row">
-                <div class="col-md-6 mx-auto">
-                    <div class="card rounded-0">
-                        <div class="card-header">
-                            <h5>Change the dates of the International Days</h5>
-                        </div>
-                        
-                        <div class="card-body">
-                           <?php 
-=======
     <div class="container">
         <div class="row">
             <div class="col-md-12 ">
@@ -34,7 +20,7 @@
 
                             <div class="card-body">
                                 <?php 
->>>>>>> 0b68ec6e8794a94f81a776ef74a3397febdce2bf
+
                             foreach ($datums as $datum) {
                             ?>
                                 <div class="form-row">
@@ -42,15 +28,11 @@
                                         <?php
                                     echo form_label("Day $i:", 'dag'.$i);
                                 ?>
-<<<<<<< HEAD
-                                </div>
-                                <div class="col-md-10">
-                                <?php
-=======
+
                                     </div>
                                     <div class="col-md-8">
                                         <?php
->>>>>>> 0b68ec6e8794a94f81a776ef74a3397febdce2bf
+
                                     echo "<input type='date' name='dag$i' id='dag$i' value='$datum->datum' min='1000-01-01' max='3000-12-31' class='form-control datums'>";
                                     $i++;
                                 ?>
@@ -59,12 +41,9 @@
                                         <button type="button" class="btn btn-danger">Delete</button>
                                     </div>
                                 </div>
-<<<<<<< HEAD
                             </div>
                             <?php
-=======
-                                <?php
->>>>>>> 0b68ec6e8794a94f81a776ef74a3397febdce2bf
+
                             }
                             ?>
                                     <div class="form-row">
