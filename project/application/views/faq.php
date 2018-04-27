a74e0974b79cb44e7c225085a95a585974a8cd13
Kim Moelants <r0615534@student.thomasmore.be>
2018-04-27 17:16:47 +0200

    faq update

--- project/application/views/faq.php
+++ project/application/views/faq.php
@@ -1,11 +1,3 @@
-
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
-
-<body>
-
 <div class="container">
   <div id="accordion">
     <div class="card">
@@ -58,12 +50,4 @@
     </div>
   </div>
 </div>
-
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
